04e988787f997aa5db40fd696cf4899496fd1301
yu_miao <yu_miao@neusoft.com>
2018-09-26 08:39:13 +0800

    commit-0926-1

--- src/com/youmeek/demo/helloworld.java
+++ src/com/youmeek/demo/helloworld.java
@@ -2,9 +2,16 @@ package com.youmeek.demo;
 
 public class helloworld {
     
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Hello World!777");
-        System.out.println("Hello World!777");
+        Integer num = 3;
+        testMethod(num);
+    }
+
+    public static  void testMethod(Integer num) {
+        if (num != null) {
+
+        }
+        System.out.println("num is :" + num);
     }
 }
